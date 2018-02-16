2bccf41bf67bd4e434ecf884297c36be905865b5
Zeeshan Ahmad <ziishaned@gmail.com>
2018-02-16 17:22:10 +0400

    Add php-7 support for chaining

--- src/Sequence/Chain.php
+++ src/Sequence/Chain.php
@@ -2,23 +2,20 @@
 
 namespace __\Sequence;
 
-use __;
-
 trait Chain
 {
     /**
-     * returns a wrapper instance, allows the value to be passed through multiple bottomline functions
+     * Returns a wrapper instance, allows the value to be passed through multiple php-lodash functions
      *
-     * __::chain([0, 1, 2, 3, null])
-     *   ->compact()
-     *   ->prepend(4)
-     *   ->value();
-     * >> [4, 1, 2, 3]
+     * @usage __::chain([0, 1, 2, 3, null])
+     *            ->compact()
+     *            ->prepend(4)
+     *            ->value();
+     *        >> [4, 1, 2, 3]
      *
      * @param mixed $initialValue
      *
      * @return mixed
-     *
      */
     public static function chain($initialValue)
     {
